c4c3ef5f8624213d7da348ced2f6fa28a2514af0
ehdi <mahdi.shariat@snapp-box.com>
2021-02-03 02:04:49 +0330

    feat(exception) : add item not found exception

---
+++ src/main/java/com/ikea/warehouse/exception/ItemNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ikea.warehouse.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ItemNotFoundException extends RuntimeException {
+  public ItemNotFoundException() {
+    super("Item not found");
+  }
+}
