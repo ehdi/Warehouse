8ef73f63cacceec32eb6ddd536b286bdb0efb101
ehdi <mahdi.shariat@snapp-box.com>
2021-02-03 18:23:06 +0330

    feat(DTO): add parent object of products and inventory

---
+++ src/main/java/com/ikea/warehouse/service/dto/ProductsListDTO.java
@@ -0,0 +1,18 @@
+package com.ikea.warehouse.service.dto;
+
+import java.util.List;
+
+public class ProductsListDTO {
+
+  private List<ProductsDTO> products;
+
+  public List<ProductsDTO> getProducts() {
+    return products;
+  }
+
+  public ProductsListDTO setProducts(
+      List<ProductsDTO> products) {
+    this.products = products;
+    return this;
+  }
+}
