6148cf02710a846ddac14eae3a24a8bb6cd6c8a9
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 01:28:55 +0330

    fix : remove null objects and fix name of json properties

--- src/main/java/com/ikea/warehouse/service/dto/ContainArticlesDTO.java
+++ src/main/java/com/ikea/warehouse/service/dto/ContainArticlesDTO.java
@@ -1,10 +1,20 @@
 package com.ikea.warehouse.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(Include.NON_NULL)
 public class ContainArticlesDTO {
 
+  @JsonIgnore
   private Long id;
+  @JsonProperty("art_id")
   private Long artId;
+  @JsonProperty("amount_of")
   private Long amountOf;
+  private ProductsDTO product;
 
   public Long getId() {
     return id;
@@ -30,6 +40,15 @@ public class ContainArticlesDTO {
 
   public ContainArticlesDTO setAmountOf(Long amountOf) {
     this.amountOf = amountOf;
+    return this;
+  }
+
+  public ProductsDTO getProduct() {
+    return product;
+  }
+
+  public ContainArticlesDTO setProduct(ProductsDTO product) {
+    this.product = product;
     return this;
   }
 }
