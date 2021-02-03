3f320fc485c31765fdc0fc950019877184937d75
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 01:18:53 +0330

    refactor : change return type of products list and implement getProductByInventoryQuantity

--- src/main/java/com/ikea/warehouse/service/ProductsService.java
+++ src/main/java/com/ikea/warehouse/service/ProductsService.java
@@ -1,12 +1,15 @@
 package com.ikea.warehouse.service;
 
-import com.ikea.warehouse.domain.Products;
 import com.ikea.warehouse.service.dto.ProductsDTO;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface ProductsService {
 
-  List<Products> getAllProducts();
+  List<ProductsDTO> getAllProducts();
   ProductsDTO save(ProductsDTO productsDTO);
+  Optional<ProductsDTO> findByName(String name);
+  Map<String, List<ProductsDTO>> getProductByInventoryQuantity();
 
 }
--- src/main/java/com/ikea/warehouse/service/impl/ProductsServiceImpl.java
+++ src/main/java/com/ikea/warehouse/service/impl/ProductsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.ikea.warehouse.service.impl;
 
+import static java.util.stream.Collectors.groupingBy;
+
 import com.ikea.warehouse.domain.Products;
+import com.ikea.warehouse.exception.ItemNotFoundException;
 import com.ikea.warehouse.repository.ProductsRepository;
+import com.ikea.warehouse.service.InventoryService;
 import com.ikea.warehouse.service.ProductsService;
+import com.ikea.warehouse.service.dto.ContainArticlesDTO;
+import com.ikea.warehouse.service.dto.InventoryDTO;
 import com.ikea.warehouse.service.dto.ProductsDTO;
 import com.ikea.warehouse.service.mapper.ProductsMapper;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.persistence.EntityGraph;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -24,32 +33,34 @@ public class ProductsServiceImpl implements ProductsService {
 
   private final ProductsRepository productsRepository;
   private final ProductsMapper productsMapper;
+  private final InventoryService inventoryService;
 
   public ProductsServiceImpl(EntityManagerFactory emf,
       ProductsRepository productsRepository,
-      ProductsMapper productsMapper) {
+      ProductsMapper productsMapper, InventoryService inventoryService) {
     this.emf = emf;
     this.productsRepository = productsRepository;
     this.productsMapper = productsMapper;
+    this.inventoryService = inventoryService;
   }
 
   @Transactional
   @Override
-  public List<Products> getAllProducts() {
+  public List<ProductsDTO> getAllProducts() {
     log.debug("Request to fetch all products");
 
     EntityManager em = emf.createEntityManager();
     em.getTransaction().begin();
 
     EntityGraph<?> entityGraph = em.createEntityGraph("graph.Products.containArticlesList");
-    TypedQuery<Products> q = em.createQuery("SELECT DISTINCT a FROM Products a", Products.class)
+    TypedQuery<Products> q = em.createQuery("SELECT a FROM Products a", Products.class)
         .setHint("javax.persistence.fetchgraph", entityGraph);
     List<Products> products = q.getResultList();
 
     em.getTransaction().commit();
     em.close();
 
-    return products;
+    return productsMapper.toDTO(products);
   }
 
   @Transactional
@@ -60,6 +71,36 @@ public class ProductsServiceImpl implements ProductsService {
         productsMapper.toEntity(productsDTO)
     );
     return productsMapper.toDTO(savedProduct);
+  }
+
+  @Transactional
+  @Override
+  public Optional<ProductsDTO> findByName(String name) {
+    return productsRepository.findByName(name)
+    .map(productsMapper::toDTO);
+  }
+
+  @Override
+  public Map<String, List<ProductsDTO>> getProductByInventoryQuantity() {
+    List<ProductsDTO> allProducts = this.getAllProducts();
+
+    return allProducts.stream()
+        .filter(products -> {
+
+          AtomicReference<Boolean> productStatus = new AtomicReference<>(true);
+          List<ContainArticlesDTO> containArticlesList = products.getContainArticlesList();
+          containArticlesList.forEach(item -> {
+
+            InventoryDTO inventoryDTO = inventoryService.findById(item.getArtId())
+                .orElseThrow(ItemNotFoundException::new);
+
+            if (inventoryDTO.getStock() < 1)
+              productStatus.set(false);
+          });
+
+          return productStatus.get();
+        })
+        .collect(groupingBy(ProductsDTO::getName));
   }
 
 }
