258acd3604b3260ab668418fb4bdeb90a519fdbf
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 01:16:14 +0330

    feat(repo) add find by name in product respository

--- src/main/java/com/ikea/warehouse/repository/ProductsRepository.java
+++ src/main/java/com/ikea/warehouse/repository/ProductsRepository.java
@@ -1,9 +1,11 @@
 package com.ikea.warehouse.repository;
 
 import com.ikea.warehouse.domain.Products;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
+  Optional<Products> findByName(String name);
 }
