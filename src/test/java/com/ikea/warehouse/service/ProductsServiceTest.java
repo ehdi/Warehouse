439fbd7f04fb9ba5c3e9bd5584a9c8790b299729
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 01:32:54 +0330

    fix : remove duplicate code of db initiate

--- src/test/java/com/ikea/warehouse/service/ProductsServiceTest.java
+++ src/test/java/com/ikea/warehouse/service/ProductsServiceTest.java
@@ -1,16 +1,8 @@
 package com.ikea.warehouse.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ikea.warehouse.domain.Products;
-import com.ikea.warehouse.service.dto.ContainArticlesDTO;
 import com.ikea.warehouse.service.dto.ProductsDTO;
-import com.ikea.warehouse.service.dto.ProductsListDTO;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,45 +18,10 @@ public class ProductsServiceTest {
   @Autowired
   private ProductsService productsService;
 
-  @Autowired
-  private ContainArticlesService containArticlesService;
-
-  @BeforeEach
-  public void setUp(){
-    loadProductsData();
-  }
-
-  private void loadProductsData(){
-    ObjectMapper mapper = new ObjectMapper();
-    TypeReference<ProductsListDTO> typeReference = new TypeReference<>(){};
-    InputStream inputStream = TypeReference.class.getResourceAsStream("/data/products.json");
-    try {
-
-      ProductsListDTO productsListDTO = mapper.readValue(inputStream,typeReference);
-      productsListDTO.getProducts().forEach(productsJson -> {
-
-        ProductsDTO productsDTO = new ProductsDTO();
-        productsDTO.setName(productsJson.getName());
-        productsService.save(productsDTO);
-
-        productsJson.getContainArticlesList().forEach(articleJson -> {
-            ContainArticlesDTO containArticlesDTO = new ContainArticlesDTO();
-            containArticlesDTO.setArtId(articleJson.getArtId());
-            containArticlesDTO.setAmountOf(articleJson.getAmountOf());
-            containArticlesDTO.setProduct(productsDTO);
-            containArticlesService.save(containArticlesDTO);
-        });
-
-      });
-    } catch (IOException e){
-      System.out.println("Unable to save products: " + e.getMessage());
-    }
-  }
-
   @Test
   public void fetchAllProducts_ShouldReturnTotalNumber() {
-    List<Products> allProducts = productsService.getAllProducts();
-    Assertions.assertEquals(8, allProducts.size());
+    List<ProductsDTO> allProducts = productsService.getAllProducts();
+    Assertions.assertEquals(6, allProducts.size());
   }
 
 
