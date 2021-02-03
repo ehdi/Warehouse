cbcf3f4a51a8e24fc8b2cf41e1f22f74223a1366
ehdi <mahdi.shariat@snapp-box.com>
2021-02-03 18:21:26 +0330

    fix(domain) : fix relation of contain articles and products

--- src/main/java/com/ikea/warehouse/domain/ContainArticles.java
+++ src/main/java/com/ikea/warehouse/domain/ContainArticles.java
@@ -2,6 +2,7 @@ package com.ikea.warehouse.domain;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,14 +13,25 @@ import javax.persistence.ManyToOne;
 public class ContainArticles {
 
   @Id
-  @GeneratedValue(strategy= GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Long artId;
   private Long amountOf;
 
-  @ManyToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "id")
+  @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
+  @JoinColumn(name = "product_id", nullable = false)
   private Products product;
+
+  public ContainArticles() {
+  }
+
+  public ContainArticles(Long id, Long artId, Long amountOf,
+      Products product) {
+    this.id = id;
+    this.artId = artId;
+    this.amountOf = amountOf;
+    this.product = product;
+  }
 
   public Long getId() {
     return id;
@@ -48,12 +60,12 @@ public class ContainArticles {
     return this;
   }
 
-  public Products getProducts() {
+  public Products getProduct() {
     return product;
   }
 
-  public ContainArticles setProducts(Products products) {
-    this.product = products;
+  public ContainArticles setProduct(Products product) {
+    this.product = product;
     return this;
   }
 }
--- src/main/java/com/ikea/warehouse/domain/Products.java
+++ src/main/java/com/ikea/warehouse/domain/Products.java
@@ -1,24 +1,41 @@
 package com.ikea.warehouse.domain;
 
-import static javax.persistence.CascadeType.ALL;
-
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedAttributeNode;
+import javax.persistence.NamedEntityGraph;
 import javax.persistence.OneToMany;
 
+@NamedEntityGraph(
+    name = "graph.Products.containArticlesList",
+    attributeNodes = @NamedAttributeNode("containArticlesList")
+)
 @Entity
 public class Products {
 
   @Id
-  @GeneratedValue(strategy= GenerationType.AUTO)
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Long id;
   private String name;
 
-  @OneToMany(mappedBy = "product", cascade = ALL)
+  @OneToMany(mappedBy = "product", fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL)
   private List<ContainArticles> containArticlesList;
+
+  public Products() {
+  }
+
+  public Products(Long id, String name,
+      List<ContainArticles> containArticlesList) {
+    this.id = id;
+    this.name = name;
+    this.containArticlesList = containArticlesList;
+  }
 
   public Long getId() {
     return id;
