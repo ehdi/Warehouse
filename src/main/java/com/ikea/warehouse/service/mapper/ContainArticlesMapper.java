105a17022f7a47523bc8e947483e3437ad8ed468
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 01:30:51 +0330

    fix : handle jackson loop by ignore product in contain article

--- src/main/java/com/ikea/warehouse/service/mapper/ContainArticlesMapper.java
+++ src/main/java/com/ikea/warehouse/service/mapper/ContainArticlesMapper.java
@@ -3,8 +3,12 @@ package com.ikea.warehouse.service.mapper;
 import com.ikea.warehouse.domain.ContainArticles;
 import com.ikea.warehouse.service.dto.ContainArticlesDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ContainArticlesMapper extends EntityMapper<ContainArticlesDTO, ContainArticles> {
+
+  @Mapping(target = "product", ignore = true)
+  ContainArticlesDTO toDTO(ContainArticles source);
 
 }
