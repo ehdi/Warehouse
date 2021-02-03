4d20b4830a4a1eaec76e4daacb9086defee2d1c3
ehdi <mahdi.shariat@snapp-box.com>
2021-02-03 18:33:53 +0330

    feat(service) : add ContainArticlesService service and implementation

---
+++ src/main/java/com/ikea/warehouse/service/ContainArticlesService.java
@@ -0,0 +1,9 @@
+package com.ikea.warehouse.service;
+
+import com.ikea.warehouse.service.dto.ContainArticlesDTO;
+
+public interface ContainArticlesService {
+
+  ContainArticlesDTO save(ContainArticlesDTO containArticlesDTO);
+
+}
