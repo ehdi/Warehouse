2d0f8a76ad0f89ff6ebac6102a9018bffa5538b1
ehdi <mahdi.shariat@snapp-box.com>
2021-02-04 03:38:18 +0330

    feat(repo) : add find all and delete all by name of product

--- src/main/java/com/ikea/warehouse/repository/ProductsRepository.java
+++ src/main/java/com/ikea/warehouse/repository/ProductsRepository.java
@@ -1,6 +1,7 @@
 package com.ikea.warehouse.repository;
 
 import com.ikea.warehouse.domain.Products;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
   Optional<Products> findByName(String name);
+  Optional<List<Products>> findAllByName(String string);
+  void deleteAllByName(String name);
 }
